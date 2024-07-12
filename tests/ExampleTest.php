d7a8374f3e68888560ee0d4d37d87962012b9b98
Luis Gonzalez <contact@inquid.co>
2024-07-11 23:36:59 -0600

    feat: Add test for custom balance model and interface implementation

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,74 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Geow\Balance\Concerns\BalanceInterface;
+use Geow\Balance\Traits\HasBalance;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 it('can test', function () {
     expect(true)->toBeTrue();
 });
+
+it ('can use any model configured in the config balance.model and implements the BalanceInterface', function () {
+    config()->set('database.default', 'testing');
+
+    // Run the test user migration
+    $migration = include __DIR__.'/create_test_user_table.php.stub';
+    $migration->up();
+
+    // Run the balance migration
+    $migration = include __DIR__.'/../database/migrations/create_balances_table.php.stub';
+    $migration->up();
+
+    config()->set('balance.model', CustomBalance::class);
+
+    $user = User::create([
+        'name' => 'John Doe',
+        'email' => 'john@doe.com',
+    ]);
+    $user->setCredit(2000);
+    $this->assertEquals(2000, $user->credit);
+    $user->increaseCredit(1000);
+    $this->assertEquals(3000, $user->credit);
+    $user->decreaseCredit(500);
+    $this->assertEquals(2500, $user->credit);
+
+    $this->assertEquals('Amount in custom format: 25', $user->creditCurrency);
+});
+
+
+// Create a model that uses the HasBalance trait and test that it can increase, decrease, set and reset the balance
+class User extends Model
+{
+    use HasBalance;
+
+    protected $fillable = ['name', 'email'];
+}
+
+
+class CustomBalance extends Model implements BalanceInterface
+{
+    protected $guarded = [];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->guarded[] = $this->primaryKey;
+        $this->table = config('balance.table');
+    }
+
+    protected function amountCurrency(): Attribute
+    {
+        $amount = $this->amount / 100;
+        return Attribute::make(
+            get: fn () => "Amount in custom format: $amount",
+        );
+    }
+
+    public function balanceable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,6 @@ namespace Geow\Balance\Tests;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Geow\Balance\BalanceServiceProvider;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Geow\Balance\Concerns\BalanceInterface;
-use Geow\Balance\Traits\HasBalance;
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Support\Number;
 
 class TestCase extends Orchestra
 {
@@ -38,44 +32,5 @@ class TestCase extends Orchestra
         $migration = include __DIR__.'/../database/migrations/create_laravel-balance_table.php.stub';
         $migration->up();
         */
-    }
-
-    // Add a test that ensures I can use any model configured in the config('balance.model') and implements the BalanceInterface
-    public function testIcanUseAnyModelConfiguredInTheConfigBalanceModelAndImplementsTheBalanceInterface()
-    {
-        config()->set('balance.model', CustomBalance::class);
-        $model = config('balance.model');
-
-        $user = new User();
-        $user->setCredit(2000);
-        $this->assertEquals(2000, $user->credit);
-        $user->increaseCredit(1000);
-        $this->assertEquals(3000, $user->credit);
-        $user->decreaseCredit(500);
-        $this->assertEquals(2500, $user->credit);
-
-        $this->assertEquals('$25.00', $user->creditCurrency);
-    }
-}
-
-// Create a model that uses the HasBalance trait and test that it can increase, decrease, set and reset the balance
-class User extends Model
-{
-    use HasBalance;
-}
-
-
-class CustomBalance extends Model implements BalanceInterface
-{
-    protected function amountCurrency(): Attribute
-    {
-        return Attribute::make(
-            get: fn () => Number::currency($this->amount / 100, 'MXN'),
-        );
-    }
-
-    public function balanceable(): MorphTo
-    {
-        return $this->morphTo();
     }
 }
