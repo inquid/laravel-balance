b32fd737369d62cc640a08e6bb288563c932908e
Luis Gonzalez <contact@inquid.co>
2024-07-11 22:39:42 -0600

    Adding new configuration

--- config/balance.php
+++ config/balance.php
@@ -1,9 +1,17 @@
 <?php
 
+use Geow\Balance\Models\Balance;
+
 return [
     /**
      * Default table name.
      * If you have changed the migration, make sure you change this too.
      */
-    'table' => 'balances'
+    'table' => 'balances',
+
+    /**
+     * Default model to use.
+     * Change this if you have extended the model.
+     */
+    'model' => Balance::class,
 ];
